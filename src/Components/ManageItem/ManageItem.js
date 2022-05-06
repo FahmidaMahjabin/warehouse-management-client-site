2bfdf7f30a6dbe2a15f6dbb0222ec4c863b7ff29
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-05-06 13:53:31 +0600

    deliver

--- src/Components/ManageItem/ManageItem.js
+++ src/Components/ManageItem/ManageItem.js
@@ -6,24 +6,36 @@ import { useParams } from 'react-router-dom';
 const ManageItem = () => {
     const { id } = useParams();
     const [item, setItem] = useState({});
+    const { name, picture, quantity, about, balance, supplier } = item;
+    
     useEffect(() => {
         axios.get(`http://localhost:5000/inventory/${id}`)
             .then(data => setItem(data.data))
-    }, [])
+    }, [item])
     const deliverItem = ()=>{
         console.log("devivered called");
+        
         if (quantity >1){
-            axios.put(`http://localhost:5000/inventory/${id}`, 
-            {
-                body:{quantity: `${quantity-1}`}
-            })
-            .then()
+            // axios(
+            // {   
+            //     method: "PUT",
+            //     url:  `http://localhost:5000/inventory/${id}`,
+            //     body: {quantity: 1}
+            // } 
+           
+            // )
+            axios.put(`http://localhost:5000/inventory/${id}`)
+            .then(res =>{
+                console.log("res received client:", res);
+                
+
+            } )
         }
         else{
             alert("quantity can't be negative")
         }    
     }
-    const { name, picture, quantity, about, balance, supplier } = item;
+    
     return (
         <Card className = "text-center mx-auto my-5" style = {{width:"500px"}}>
             <Card.Img variant="top" src={picture} />
